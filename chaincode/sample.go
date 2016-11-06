75f2d2fbbe9dec7d0fd54783554c09d06ca9bebd
Henry Bond <habond@gmail.com>
2016-11-06 18:13:18 -0500

    Update sample.go

--- chaincode/sample.go
+++ chaincode/sample.go
@@ -110,6 +110,6 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	valAsbytes = []byte("dummy value")
+	/*valAsbytes = []byte("dummy value")*/
 	return valAsbytes, nil
 }
