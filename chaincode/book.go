6f9dff8b1e5d483fa300275c7865d8a21d1e3aa4
Henry Bond <habond@gmail.com>
2016-11-19 08:22:29 -0500

    batch check-in

--- chaincode/book.go
+++ chaincode/book.go
@@ -88,7 +88,8 @@ type Order struct {
 // Public Functions
 
 func (c ChaincodeFunctions) GetRole() ([]byte, error) {
-    role, err := c.stub.GetCallerCertificate();
+    //role, err := c.stub.GetCallerCertificate();
+    role, err := c.stub.ReadCertAttribute("role")
     if err != nil { return nil, errors.New("Couldn't get attribute 'role'. Error: " + err.Error()) }
     str := base64.StdEncoding.EncodeToString(role)
 	return []byte(str), nil
